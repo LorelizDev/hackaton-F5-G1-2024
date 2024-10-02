6041381803e87ff60bd79223ce7671858ccd10e3
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-10-02 21:43:40 +0200

    style:add  home text home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+<div className="relative max-sm:bg-light max-sm:w-screen max-sm:h-screen">
+
+  <div className="flex max-sm:ml-6">
+    <p className="max-sm:text-5xl max-sm:font-mainFont max-sm:font-black max-sm:text-dark max-sm:mt-20">
+      ¿Necesitas<br/> hablar?
+    </p>
+  </div>
+  <div className="flex max-sm:ml-6">
+    <p className="max-sm:text-2xl max-sm:font-mainFont max-sm:font-black max-sm:text-medium max-sm:mt-1">Estamos para<br/> ayudarte</p>
+  </div>
+  
+</div>
+
+
+
+    </>
   )
 }
 
