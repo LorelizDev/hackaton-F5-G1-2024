6efd25802a55f6a7dfa718b5848725533d6d374d
mariandrean <m.andreaan@gmail.com>
2024-10-02 13:30:24 +0200

    feat(src):add-router,homepage,layoutpublic,errorboundary

---
+++ src/router/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import LayoutPublic from "../layout/LayoutPublic";
+import ErrorBoundary from "../components/ErrorBoundary";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutPublic />,
+    errorElement: <ErrorBoundary />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      }
+    ]
+  }  
+]);
+
+export default router;
