f5858fb3e0aa10f86b4f8043063945b5b8d357f8
LorelizDev <loreliz.dev@gmail.com>
2024-10-03 15:56:29 +0200

    fix(styles): adjust mobile layout for MySpace and Community pages

--- src/pages/Comunity.jsx
+++ src/pages/Comunity.jsx
@@ -78,15 +78,15 @@ const Community = () => {
       </div>
       <div className='flex items-center w-3/5 mx-auto my-6'>
         <SearchBar onSearch={handleSearch} />
-        <button className='bg-medium rounded-full w-8 h-8'>
+        <button className='bg-medium rounded-full w-8 h-8 ml-2'>
           <GoPencil className='m-auto text-white' />
         </button>
       </div>
-      <div className="flex justify-center my-4">
+      <div className="flex flex-wrap my-4 justify-center">
         {categories.map((category) => (
           <button
             key={category}
-            className={`px-4 py-2 mx-2 rounded-xl 
+            className={`mb-1 px-2 py-2 mx-1 rounded-xl 
               ${activeCategory === category ? 'bg-medium text-white' : 'bg-gray-300 text-black'}`}
             onClick={() => filterPostsByCategory(category)}
           >
--- src/pages/MySpace.jsx
+++ src/pages/MySpace.jsx
@@ -95,10 +95,10 @@ const MySpace = () => {
             </div>
             <div>
               <h3>¿Cómo te sientes hoy?</h3>
-              <div className=' flex gap-4 '>
+              <div className='flex flex-wrap justify-evenly'>
                 {moods.map((mood) => (
                   <div key={mood.name}>
-                    <button className={`${mood.color} m-2 p-2 rounded-xl text-white text-4xl`}>
+                    <button className={`${mood.color} my-2 p-2 rounded-xl text-white text-4xl`}>
                       {mood.icon}
                     </button>
                     <p className='text-xs text-center'>{mood.name}</p>
@@ -107,17 +107,17 @@ const MySpace = () => {
               </div>
             </div>
           </div>
-          <div className='flex items-center max-w-96 mx-auto my-6'>
+          <div className='flex items-center w-3/5 mx-auto my-6'>
             <SearchBar onSearch={handleSearch} />
-            <button className='bg-medium rounded-full w-8 h-8'>
+            <button className='bg-medium rounded-full w-8 h-8 ml-2'>
               <GoPencil className='m-auto text-white' />
             </button>
           </div>
-          <div className="flex justify-center my-4">
+          <div className="flex flex-wrap my-4 justify-center">
             {categories.map((category) => (
               <button
                 key={category}
-                className={`px-4 py-2 mx-2 rounded-xl 
+                className={`mb-1 px-2 py-2 mx-1 rounded-xl 
                   ${activeCategory === category ? 'bg-medium text-white' : 'bg-gray-300 text-black'}`}
                 onClick={() => filterPostsByCategory(category)}
               >
