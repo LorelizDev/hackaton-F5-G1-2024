9f1ab78eb8069e30f37066aaad410e92aded5675
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-10-03 16:43:01 +0200

    Add: Home completed on desktop and mobile

--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import LogOutButton from "../components/LogOutButton";
 import { useUserContext } from '../context/UserContext';
-import Navbar from "../components/NavBar";
+
 
 const LayoutPublic = () => {
   const { userAuth } = useUserContext();
   return (
     <>
-      <Navbar />
+      
       {userAuth && <LogOutButton />}
       <Outlet />
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,34 +13,36 @@ function Home() {
 
     {/*div 1 */}
   <div className="flex max-sm:ml-6">
-      <p className="lg:text-5xl lg:font-mainFont lg:font-bold lg:text-dark lg:mt-20 max-sm:text-pretty max-sm:text-5xl max-sm:font-mainFont max-sm:font-black max-sm:text-dark max-sm:mt-10">
+      <p className="lg:text-5xl lg:ml-20 lg:font-mainFont lg:font-bold lg:text-dark lg:mt-20 max-sm:text-pretty max-sm:text-5xl max-sm:font-mainFont max-sm:font-black max-sm:text-dark max-sm:mt-10">
         ¿Necesitas hablar?
       </p>
   </div>
 
     {/* div 2 */}
   <div className="flex max-sm:ml-6">
-      <p className="max-sm:text-2xl max-sm:font-mainFont max-sm:font-black max-sm:text-medium max-sm:mt-1">Estamos para<br/> ayudarte</p>      
+      <p className="lg:text-3xl lg:ml-20 lg:mt-6  lg:font-mainFont lg:font-bold lg:text-medium max-sm:text-4xl max-sm:font-mainFont  max-sm:font-black max-sm:text-medium max-sm:mt-1">Estamos para ayudarte</p>      
   </div>
 
     {/* div 3 */}
   <div className="flex max-sm:bg-light max-sm:justify-center">
-      <p className="font-mainFont  max-sm:text-2xl max-sm:mr-8 max-sm:font-medium max-sm:ml-6 max-sm:text-left max-sm:text-prettyleading-relaxed text-dark max-w-md max-sm:mt-6 ">
+      <p className=" lg:ml-20 lg:text-black lg:font-medium lg:mt-6 lg:font-mainFont  max-sm:text-2xl max-sm:mr-8 max-sm:font-medium max-sm:ml-6 max-sm:text-left max-sm:text-prettyleading-relaxed max-sm:text-dark max-w-md max-sm:mt-6 ">
         Somos una organización sin fines de lucro dedicada a la protección del alma, centrada en la prevención del suicidio y el apoyo a personas en situación de depresión.
         Creemos en ser una armadura emocional, creando un refugio seguro donde cada persona se sienta escuchada, apoyada y protegida.
         <br/><br/>
         Juntos, cuidamos las alma vulnerables y construimos una comunidad más fuerte y solidaria.
+        <br/><br7/><br/>
+        Explora nuestros distintos recursos gratuitos, o si estás en un momento de crisis puedes contactar directamente con alguno de nuestros ‘Saviours’ y/o recibir ayuda adicional y urgente. 
       </p>
   </div>
 
     {/* div 4 */}
-  <div className="flex max-sm:bg-light justify-center ">
-      <img className="max-sm:w-96 max-sm:ml-6 max-sm:mr-6 max-sm:h-64 max-sm:rounded-3xl  max-sm:mt-6" src={imgHome} alt="" />
+  <div className="flex lg:mr-20 lg:justify-end lg:-mt-96 max-sm:bg-light max-sm:justify-center ">
+      <img className="lg:w-[540px] lg:h-96 lg:rounded-3xl  max-sm:w-96 max-sm:ml-6 max-sm:mr-6 max-sm:h-64 max-sm:rounded-3xl  max-sm:mt-6" src={imgHome} alt="" />
   </div>
 
     {/* div 5 ( Button Component )*/}
-  <div className="flex justify-center max-sm:bg-light">
-      <HelpButton className="max-sm:w-96" />
+  <div className="flex max-sm:justify-center max-sm:bg-light">
+      <HelpButton className="w-96" />
   </div>
   
 </div>
