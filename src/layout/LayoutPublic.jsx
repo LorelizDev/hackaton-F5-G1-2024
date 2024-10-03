580cf98bb7074e9284583199cfd8e4884e9dbae4
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-10-03 17:34:13 +0200

    merge: branch dev with feature-home

--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { useUserContext } from '../context/UserContext';
 import Footer from '../components/Footer.jsx'
-import Navbar from "../components/NavBar";
+
 
 const LayoutPublic = () => {
 
   return (
     <>
-      <Navbar />
-      {userAuth && <LogOutButton />}
+     
       <Outlet />
       <Footer />
     </>
