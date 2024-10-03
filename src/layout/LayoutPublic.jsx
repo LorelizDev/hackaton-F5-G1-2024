e6974c42fa55f158d2618c4488ba24fe9cb45b65
LuceroHernandez <lu.hernandez726@gmail.com>
2024-10-03 14:22:47 +0200

    Feat(Client):Update LayoutPublic

--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -1,21 +1,15 @@
 import { Outlet } from "react-router-dom";
 import LogOutButton from "../components/LogOutButton";
 import { useUserContext } from '../context/UserContext';
-import Footer from '../components/Footer';
 
 const LayoutPublic = () => {
   const { userAuth } = useUserContext();
   return (
     <>
       {userAuth && <LogOutButton />}
-      (
-    <>
       <Outlet />
-    < />
-      <Footer />
     </>
   )
-  );
 }
 
 export default LayoutPublic;
