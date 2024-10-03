7a81fc3ff53621ce1d8e296d9a4f2202d1ab6a61
mariandrean <m.andreaan@gmail.com>
2024-10-03 19:23:51 +0200

    fix(navbar):add-close-button-to-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,24 +11,25 @@ const Navbar = () => {
     setOpen(!open);
   };
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
-        setOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [menuRef]);
+  /*   useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+          setOpen(false);
+        }
+      };
+  
+      document.addEventListener('mousedown', handleClickOutside);
+  
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [menuRef]); */
 
   return (
     <>
       <div className="flex justify-end relative z-10">
-        <button onClick={toggleMenu} className="flex p-2 text-dark focus:outline-none">
+        <button onClick={toggleMenu} className="flex p-2 text-dark focus:outline-none z-20">
+          {!open?
           <svg
             className="w-6 h-6"
             fill="none"
@@ -38,10 +39,15 @@ const Navbar = () => {
           >
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
           </svg>
+          :
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"></path>
+          </svg>
+          }
         </button>
         {open && (
           <div ref={menuRef} className="top-5 max-sm:w-screen max-sm:h-screen absolute right-0 mt-2 w-96 bg-light rounded-lg ">
-            
+
             <ul className="flex flex-col p-2">
               {
                 !userAuth && <li className="py-2 px-4 hover:bg-gray-200 font-mainFont font-black text-3xl text-dark"><a href="/login">Iniciar sesión</a></li>
