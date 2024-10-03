2d67af9e457a0bd96c8082baa3627fd40a1a8d64
DarthVada36 <velazquez.vada@outlook.com>
2024-10-03 21:10:46 +0200

    fix: home Media Query mobile text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import imgHome from '../assets/imgHome.jpg';
+import HelpButton from '../components/HelpButton';
 
 function Home() {
   return (
     <>
       {/* Beginning of div container */}
-      <div className="relative max-sm:bg-light max-sm:w-screen max-sm:h-screen flex flex-col lg:flex-row lg:gap-0 lg:justify-between lg:items-center px-4 lg:px-20 py-8"> {/* Añadido padding aquí */}
+      <div className="relative max-sm:bg-light max-sm:w-screen min-h-screen flex flex-col lg:flex-row lg:gap-0 lg:justify-between lg:items-center px-4 lg:px-20 py-8">
 
         {/* Div 1 */}
-        <div className="flex flex-col max-sm:ml-6 lg:w-1/2 lg:mr-2"> 
+        <div className="flex flex-col max-sm:ml-6 lg:w-1/2 lg:mr-2">
           <p className="lg:text-5xl lg:ml-4 lg:font-mainFont lg:font-bold lg:text-dark max-sm:text-pretty max-sm:text-5xl max-sm:font-mainFont max-sm:font-black max-sm:text-dark max-sm:mt-10">
             ¿Necesitas hablar?
           </p>
@@ -23,10 +24,15 @@ function Home() {
             Somos una organización sin fines de lucro dedicada a la protección del alma, centrada en la prevención del suicidio y el apoyo a personas en situación de depresión.
             Creemos en ser una armadura emocional, creando un refugio seguro donde cada persona se sienta escuchada, apoyada y protegida.
             <br /><br />
-            Juntos, cuidamos las alma vulnerables y construimos una comunidad más fuerte y solidaria.
+            Juntos, cuidamos las almas vulnerables y construimos una comunidad más fuerte y solidaria.
             <br /><br />
             Explora nuestros distintos recursos gratuitos, o si estás en un momento de crisis puedes contactar directamente con alguno de nuestros ‘Saviours’ y/o recibir ayuda adicional y urgente.
           </p>
+
+          {/* Div 5 ( Button Component ) */}
+          <div className="flex justify-center mt-4">
+            <HelpButton className="w-96" />
+          </div>
         </div>
 
         {/* Div 4 */}
@@ -34,7 +40,6 @@ function Home() {
           <img className="lg:w-[540px] lg:h-96 lg:rounded-3xl max-sm:w-96 max-sm:h-auto max-sm:rounded-3xl max-sm:mt-6" src={imgHome} alt="Imagen de ayuda" />
         </div>
       </div>
-
       {/* End of div container */}
     </>
   );
